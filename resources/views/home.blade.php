47b52d47a3b83fb433c3b7707f6fa2528bddea6f
Luigi-Iorio <luigi0iorio@gmail.com>
2024-01-27 15:29:35 +0100

    feat: scaffolding of main component - home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.main')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel Comics</title>
-    @vite('resources/js/app.js')
-</head>
-
-<body>
-</body>
-
-</html>
+@section('main')
+    {{-- main --}}
+    <main>
+    </main>
+    {{-- /main --}}
+@endsection
