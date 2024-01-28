acfe199b6d3b0874d5afcc6ef8a5ecea0051c712
Luigi-Iorio <luigi0iorio@gmail.com>
2024-01-28 20:04:58 +0100

    feat: home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.main')
+
+@section('main')
+    {{-- main --}}
+    <main>
+        <div class="main home">
+            <h1>DC Comics</h1>
+            <h3>Leader nell'industria dei fumetti</h3>
+            <p>Siamo il punto di riferimento per storie epiche e personaggi leggendari. Batman, Superman, Wonder Woman e
+                molti
+                altri hanno trovato casa nei nostri fumetti, diventando pilastri culturali che attraversano generazioni. La
+                DC
+                Comics è sinonimo di innovazione, intrattenimento di alta qualità e un legame duraturo con il suo pubblico
+                globale.<br>Benvenuti nel mondo della DC Comics, dove le storie diventano leggenda.</p>
+        </div>
+
+    </main>
+    {{-- /main --}}
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,33 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    $headerLinks = ["characters", "comics", "movies", "tv", "games", "collectibles", "videos", "fans", "news", "shop"];
+    $footerLinks = [
+        [
+            "title" => "dc comics",
+            "links" => ["Character", "Comics", "Movies", "TV", "Games", "Videos", "News"]
+        ],
+        [
+            "title" => "shops",
+            "links" => ["Shop DC", "Shop DC Collectibles"]
+        ],
+        [
+            "title" => "dc",
+            "links" => ["Terms Of Use", "Privacy policy", "Ad Choices", "Advertising", "Jobs", "Subscriptions", "Talent Workshops", "CPSC Certificates", "Ratings", "Shop Help", "Contacts Us"]
+        ],
+        [
+            "title" => "sites",
+            "links" => ["DC", "MAD Magazine", "DC Kids", "DC Universe", "DC Power Visa"]
+        ]
+    ];
+    $socialIcons = [
+        "resources/img/footer-facebook.png", "resources/img/footer-twitter.png", "resources/img/footer-youtube.png", "resources/img/footer-pinterest.png", "resources/img/footer-periscope.png",
+    ];
+
+    return view('home', compact('headerLinks', 'footerLinks', 'socialIcons'));
+});
+
 Route::get('/comics', function () {
     $headerLinks = ["characters", "comics", "movies", "tv", "games", "collectibles", "videos", "fans", "news", "shop"];
     $data = config("comics_db");
