e5a00a0cd4c8c3ad53885f4853121fe943ca7429
Luigi-Iorio <luigi0iorio@gmail.com>
2024-01-28 11:10:35 +0100

    feat: comics in main component

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,23 @@
 @section('main')
     {{-- main --}}
     <main>
+        <ul class="cont">
+            {{-- comic --}}
+            @foreach ($data as $comic)
+                <li class="card">
+                    <a href="#">
+                        <div class="img-cont">
+                            <img src="{{ $comic['thumb'] }}" alt="title">
+                        </div>
+                        <h3>{{ $comic['series'] }}</h3>
+                    </a>
+                </li>
+            @endforeach
+            {{-- /comic --}}
+            {{-- button --}}
+            <button>Load More</button>
+            {{-- button --}}
+        </ul>
     </main>
     {{-- /main --}}
 @endsection
